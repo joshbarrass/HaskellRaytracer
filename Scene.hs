bc243d020cd8a6612c118142f8e90dec20320604
Joshua Barrass <josh.barrass.work@gmail.com>
2022-01-29 01:03:48 +0000

    Random rays for anti-aliasing

--- Scene.hs
+++ Scene.hs
@@ -13,6 +13,11 @@ import Hit
 
 import Data.Maybe
 import Data.List
+import System.Random
+-- import System.Random.PCG.Fast.Pure (create, uniformF)
+
+-- traceShowId :: a -> a
+-- traceShowId x = x
 
 defaultBackground :: Ray -> Pixel
 defaultBackground ray = let
@@ -21,7 +26,7 @@ defaultBackground ray = let
   t = (y + 1.0) * 0.5
   in ((1-t) + t*0.5, (1-t) + t*0.7, 1)
 
-data Scene = Scene { objects :: [Object], background :: Ray -> Pixel,  bottomLeft :: Vec3, screenSize :: (Vec3, Vec3), seed :: Int}
+data Scene = Scene { objects :: [Object], background :: Ray -> Pixel,  bottomLeft :: Vec3, screenSize :: (Vec3, Vec3), aa :: Int, seed :: Int}
 
 defaultBGScene objects = Scene objects defaultBackground
 
@@ -29,15 +34,28 @@ render :: Scene -> (Int, Int) -> Image
 render scene (width, height) = let
   w = fromIntegral width :: Float
   h = fromIntegral height :: Float
-  u i = scale (fromIntegral i / w) (fst $ screenSize scene)
-  v j = scale (fromIntegral j / h) (snd $ screenSize scene)
+  u i = scale (i / w) (fst $ screenSize scene)
+  v j = scale (j / h) (snd $ screenSize scene)
+  ns = aa scene
 
-  targets = [[bottomLeft scene `vadd` u i `vadd` v j | i <- [0..width-1]] | j <- [height-1, height-2..0]]
+  randomList = if ns > 1
+    then getRandomList $ seed scene
+    else [0.5 | _ <- [1..]]
+
+  totalIndex i j = round w * j + i :: Int
+  randomNums i j = take (ns * 2) $ drop (totalIndex i j * ns * 2) randomList
+
+  targets = [[[bottomLeft scene `vadd` u (fromIntegral i + rx) `vadd` v (fromIntegral j + ry)
+              | (rx, ry) <- splitZip $ randomNums i j]
+             | i <- [0..width-1]]
+            | j <- [height-1, height-2..0]]
   -- rays = [[pointFromTo (0,0,0) t | t <- targetRow] | targetRow <- targets]
-  rays = map (map (pointFromTo (0,0,0))) targets
+  rays = map (map (map (pointFromTo (0,0,0)))) targets 
 
-  -- in [[color scene ray | ray <- rowOfRays] | rowOfRays <- rays]
-  in map (map (color scene)) rays
+  in [[fromIntegral ns `vdiv` foldl1 vadd [color scene ray | ray <- aaRays]
+      | aaRays <- rowOfRays]
+     | rowOfRays <- rays]
+  -- in map (map (map (color scene)) rays
 
 color :: Scene -> Ray -> Pixel
 color scene ray = let
@@ -47,3 +65,15 @@ color scene ray = let
                   xs -> let closest = head (sortOn t xs)
                             n = normal closest
                             in 0.5 `scale ` (n `vadd` (1, 1, 1))
+
+getRandomList :: Int -> [Float]
+getRandomList seed = randoms (mkStdGen seed) :: [Float]
+-- getRandomList seed = let
+  -- g = create seed
+  -- in [uniformF g | _ <- [1..]]
+
+splitZip :: [a] -> [(a, a)]
+splitZip xs = let
+  l = length xs
+  midpoint = (l `div` 2)
+  in zip (take midpoint xs) (drop midpoint xs)
--- raytracer.hs
+++ raytracer.hs
@@ -5,6 +5,7 @@ import Objects
 main :: IO ()
 main = do
   let
-    scene = defaultBGScene [Sphere 0.5 (0, 0, -1)] (-2, -1, -1) ((4, 0, 0), (0, 2, 0))
+    seed = 1234554321
+    scene = defaultBGScene [Sphere 0.5 (0, 0, -1)] (-2, -1, -1) ((4, 0, 0), (0, 2, 0)) 50 seed
     dims = (200, 100)
     in Image.savePPM (render scene dims) "test.ppm"
